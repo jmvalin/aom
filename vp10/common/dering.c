a4894436e4bd93cd2d3d84fba5cd0460f6ddd4ef
Jean-Marc Valin <jmvalin@jmvalin.ca>
2016-01-31 00:31:27 -0500

    Whole frame deringing from superblocks

--- vp10/common/dering.c
+++ vp10/common/dering.c
@@ -135,7 +135,53 @@ void vp10_dering_rows(YV12_BUFFER_CONFIG *frame_buffer, VP10_COMMON *cm,
 
 void vp10_dering_frame(YV12_BUFFER_CONFIG *frame, VP10_COMMON *cm,
                        MACROBLOCKD *xd, int level) {
+#if 1
+  int r, c;
+  int sbr, sbc;
+  int nhsb, nvsb;
+  int16_t *src, *dst;
+  unsigned char *bskip;
+  int dir[OD_DERING_NBLOCKS][OD_DERING_NBLOCKS] = {{0}};
+  int stride;
+  src = malloc(sizeof(*src)*cm->mi_rows*cm->mi_cols*64);
+  dst = malloc(sizeof(*dst)*cm->mi_rows*cm->mi_cols*64);
+  bskip = malloc(sizeof(*bskip)*cm->mi_rows*cm->mi_cols);
   vp10_setup_dst_planes(xd->plane, frame, 0, 0);
-
+  stride = 8*cm->mi_cols;
+  for (r = 0; r < 8*cm->mi_rows; ++r) {
+    for (c = 0; c < 8*cm->mi_cols; ++c) {
+      dst[r * stride + c] = src[r * stride + c] = xd->plane[0].dst.buf[r * xd->plane[0].dst.stride + c];
+    }
+  }
+  for (r = 0; r < cm->mi_rows; ++r) {
+    for (c = 0; c < cm->mi_cols; ++c) {
+#if 0
+      const MB_MODE_INFO *mbmi =
+          &cm->mi_grid_visible[r * cm->mi_stride + c]->mbmi;
+      bskip[r * cm->mi_cols + c] = mbmi->skip;
+#else
+      bskip[r * cm->mi_cols + c] = 0;
+#endif
+    }
+  }
+  nvsb = cm->mi_rows/8;
+  nhsb = cm->mi_cols/8;
+  for (sbr = 0; sbr < nvsb; sbr++) {
+    for (sbc = 0; sbc < nhsb; sbc++) {
+      od_dering(&OD_DERING_VTBL_C, dst + sbr*stride*64 + sbc*64, cm->mi_cols*8, src + sbr*stride*64 + sbc*64, cm->mi_cols*8, 6,
+          sbc, sbr, nhsb, nvsb, 0, dir, 0,
+          bskip, 0, level, OD_DERING_NO_CHECK_OVERLAP);
+    }
+  }
+  for (r = 0; r < 8*cm->mi_rows; ++r) {
+    for (c = 0; c < 8*cm->mi_cols; ++c) {
+      xd->plane[0].dst.buf[r * xd->plane[0].dst.stride + c] = dst[r * stride + c];
+    }
+  }
+  free(src);
+  free(dst);
+  free(bskip);
+#else
   vp10_dering_rows(frame, cm, xd, 0, cm->mi_rows, level);
+#endif
 }
