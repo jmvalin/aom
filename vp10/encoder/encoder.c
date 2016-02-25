8076d5c47a867440721adc09282bf068ea8d40cc
Jean-Marc Valin <jmvalin@jmvalin.ca>
2016-02-24 19:36:34 -0500

    Adds the Daala deringing filter as experimental
    The deringing filter documentation is in:
    J.-M. Valin, The Daala Directional Deringing Filter, arXiv:1602.05975 [cs.MM],
    2016.
    http://arxiv.org/pdf/1602.05975.pdf
    
    ntt-short1 results:
              MEDIUM (%) HIGH (%)
        PSNR  -2.488088 -2.003235
     PSNRHVS  -1.588932 -0.797850
        SSIM  -1.522767 -1.287861
    FASTSSIM   4.307822  3.983496
    
    subset1 improvement is around 2-3% on PSNR (but the rate is mostly outside
    the AWCY testing range)
    
    Change-Id: Ic02344ce9faa509f5c3a50a0fb7a7b84a7977e72

--- vp10/encoder/encoder.c
+++ vp10/encoder/encoder.c
@@ -15,6 +15,9 @@
 #include "./vpx_config.h"
 
 #include "vp10/common/alloccommon.h"
+#if CONFIG_DERING
+#include "vp10/common/dering.h"
+#endif  // CONFIG_DERING
 #include "vp10/common/filter.h"
 #include "vp10/common/idct.h"
 #include "vp10/common/reconinter.h"
@@ -2425,6 +2428,15 @@ static void loopfilter_frame(VP10_COMP *cpi, VP10_COMMON *cm) {
     else
       vp10_loop_filter_frame(cm->frame_to_show, cm, xd, lf->filter_level, 0, 0);
   }
+
+#if CONFIG_DERING
+  if (is_lossless_requested(&cpi->oxcf)) {
+    cm->dering_level = 0;
+  } else {
+    cm->dering_level = vp10_dering_search(cm->frame_to_show, cpi->Source, cm, xd);
+    vp10_dering_frame(cm->frame_to_show, cm, xd, cm->dering_level);
+  }
+#endif  // CONFIG_DERING
 
   vpx_extend_frame_inner_borders(cm->frame_to_show);
 }
