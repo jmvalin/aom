e460277460e8f5a043c75063446f74995bd295fb
Jean-Marc Valin <jmvalin@mozilla.com>
2016-08-11 17:35:55 -0400

    Direction 5 works
    Change-Id: I445dd3adbf8c239f454dd94d79233e7448c5b4b6

--- av1/common/od_dering.c
+++ av1/common/od_dering.c
@@ -43,6 +43,25 @@ const int OD_DIRECTION_OFFSETS_TABLE[8][3] = {
 #include <tmmintrin.h>
 #include "aom_dsp/x86/inv_txfm_sse2.h"
 
+static INLINE __m128i fold_mul_and_sum(__m128i partial7a, __m128i partial7b) {
+  __m128i tmp;
+  partial7b = _mm_shuffle_epi8(partial7b, _mm_set_epi8(15, 14, 1, 0, 3, 2, 5, 4,
+      7, 6, 9, 8, 11, 10, 13, 12));
+  tmp = partial7a;
+  partial7a = _mm_unpacklo_epi16(partial7a, partial7b);
+  partial7b = _mm_unpackhi_epi16(tmp, partial7b);
+  partial7a = _mm_madd_epi16(partial7a, partial7a);
+  partial7b = _mm_madd_epi16(partial7b, partial7b);
+  partial7a = _mm_mullo_epi32(partial7a, _mm_set_epi32(210, 420, 0, 0));
+  partial7b = _mm_mullo_epi32(partial7b, _mm_set_epi32(105, 105, 105, 140));
+  partial7a = _mm_add_epi32(partial7a, partial7b);
+  partial7a = _mm_add_epi32(partial7a,
+      _mm_unpackhi_epi64(partial7a, partial7a));
+  partial7a = _mm_add_epi32(partial7a,
+      _mm_shufflelo_epi16(partial7a, _MM_SHUFFLE(1, 0, 3, 2)));
+  return partial7a;
+}
+
 int od_dir_find8_sse2(const od_dering_in *img, int stride, int32_t *var,
                         int coeff_shift) {
   int i;
@@ -183,18 +202,8 @@ int od_dir_find8_sse2(const od_dering_in *img, int stride, int32_t *var,
     }
     printf("\n");
   }
-  partial7b = _mm_shuffle_epi8(partial7b, _mm_set_epi8(15, 14, 1, 0, 3, 2, 5, 4, 7, 6, 9, 8, 11, 10, 13, 12));
-  tmp = partial7a;
-  partial7a = _mm_unpacklo_epi16(partial7a, partial7b);
-  partial7b = _mm_unpackhi_epi16(tmp, partial7b);
-  partial7a = _mm_madd_epi16(partial7a, partial7a);
-  partial7b = _mm_madd_epi16(partial7b, partial7b);
-  partial7a = _mm_mullo_epi32(partial7a, _mm_set_epi32(210, 420, 0, 0));
-  partial7b = _mm_mullo_epi32(partial7b, _mm_set_epi32(105, 105, 105, 140));
-  partial7a = _mm_add_epi32(partial7a, partial7b);
-  partial7a = _mm_add_epi32(partial7a, _mm_unpackhi_epi64(partial7a, partial7a));
-  partial7a = _mm_add_epi32(partial7a, _mm_shufflelo_epi16(partial7a, _MM_SHUFFLE(1, 0, 3, 2)));
-  //printf("%d\n", _mm_cvtsi128_si32(partial7a));
+  partial7a = fold_mul_and_sum(partial7a, partial7b);
+  partial5a = fold_mul_and_sum(partial5a, partial5b);
 
   array_transpose_8x8(lines, tlines);
   for (i = 0; i < 8; i++) {
@@ -218,7 +227,7 @@ int od_dir_find8_sse2(const od_dering_in *img, int stride, int32_t *var,
       //partial[2][i] += x;
       partial[3][3 + i - j / 2] += x;
       //partial[4][7 + i - j] += x;
-      partial[5][3 - i / 2 + j] += x;
+      //partial[5][3 - i / 2 + j] += x;
       //partial[6][j] += x;
       //partial[7][i / 2 + j] += x;
     }
@@ -260,6 +269,7 @@ int od_dir_find8_sse2(const od_dering_in *img, int stride, int32_t *var,
   }
   //printf("%d\n\n", cost[7]);
   cost[7] = _mm_cvtsi128_si32(partial7a);
+  cost[5] = _mm_cvtsi128_si32(partial5a);
   for (i = 0; i < 8; i++) {
     if (cost[i] > best_cost) {
       best_cost = cost[i];
