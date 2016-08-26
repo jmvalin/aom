0b9627afc0cb1c92d167b927a9fa12bde1b3fcdf
Jean-Marc Valin <jmvalin@mozilla.com>
2016-08-26 16:44:33 -0400

    superblock copy function
    Change-Id: I7a514d4edb4ee6ab5c9f4f9d2239129a6472e516

--- av1/common/dering.c
+++ av1/common/dering.c
@@ -45,13 +45,84 @@ int sb_all_skip(const AV1_COMMON *const cm, int mi_row, int mi_col) {
   return skip;
 }
 
-static void copy_sb16_8(uint8_t *dst, int dstride, od_dering_in *src,
+#include <emmintrin.h>
+static void copy_sb16_8(uint8_t * restrict dst, int dstride, od_dering_in * restrict src,
                         int sstride, int vsize, int hsize)
 {
   int r, c;
-  for (r = 0; r < vsize; ++r) {
-    for (c = 0; c < hsize; ++c) {
-        dst[dstride * r + c] = src[sstride * r + c];
+  if (hsize == 64) {
+    for (r = 0; r < vsize; ++r) {
+      __m128i tmp;
+      tmp = _mm_loadu_si128((__m128i*)&src[0]);
+      tmp = _mm_packus_epi16(tmp, tmp);
+      _mm_storel_epi64((__m128i*)&dst[0], tmp);
+
+      tmp = _mm_loadu_si128((__m128i*)&src[8]);
+      tmp = _mm_packus_epi16(tmp, tmp);
+      _mm_storel_epi64((__m128i*)&dst[8], tmp);
+
+      tmp = _mm_loadu_si128((__m128i*)&src[16]);
+      tmp = _mm_packus_epi16(tmp, tmp);
+      _mm_storel_epi64((__m128i*)&dst[16], tmp);
+
+      tmp = _mm_loadu_si128((__m128i*)&src[24]);
+      tmp = _mm_packus_epi16(tmp, tmp);
+      _mm_storel_epi64((__m128i*)&dst[24], tmp);
+
+      tmp = _mm_loadu_si128((__m128i*)&src[32]);
+      tmp = _mm_packus_epi16(tmp, tmp);
+      _mm_storel_epi64((__m128i*)&dst[32], tmp);
+
+      tmp = _mm_loadu_si128((__m128i*)&src[40]);
+      tmp = _mm_packus_epi16(tmp, tmp);
+      _mm_storel_epi64((__m128i*)&dst[40], tmp);
+
+      tmp = _mm_loadu_si128((__m128i*)&src[48]);
+      tmp = _mm_packus_epi16(tmp, tmp);
+      _mm_storel_epi64((__m128i*)&dst[48], tmp);
+
+      tmp = _mm_loadu_si128((__m128i*)&src[56]);
+      tmp = _mm_packus_epi16(tmp, tmp);
+      _mm_storel_epi64((__m128i*)&dst[56], tmp);
+
+      src += sstride;
+      dst += dstride;
+    }
+  } else if (hsize == 32) {
+    for (r = 0; r < vsize; ++r) {
+      __m128i tmp;
+      tmp = _mm_loadu_si128((__m128i*)&src[0]);
+      tmp = _mm_packus_epi16(tmp, tmp);
+      _mm_storel_epi64((__m128i*)&dst[0], tmp);
+
+      tmp = _mm_loadu_si128((__m128i*)&src[8]);
+      tmp = _mm_packus_epi16(tmp, tmp);
+      _mm_storel_epi64((__m128i*)&dst[8], tmp);
+
+      tmp = _mm_loadu_si128((__m128i*)&src[16]);
+      tmp = _mm_packus_epi16(tmp, tmp);
+      _mm_storel_epi64((__m128i*)&dst[16], tmp);
+
+      tmp = _mm_loadu_si128((__m128i*)&src[24]);
+      tmp = _mm_packus_epi16(tmp, tmp);
+      _mm_storel_epi64((__m128i*)&dst[24], tmp);
+
+      src += sstride;
+      dst += dstride;
+    }
+  } else {
+    for (r = 0; r < vsize; ++r) {
+      for (c = 0; c < hsize - 7; c+=8) {
+        __m128i tmp;
+        tmp = _mm_loadu_si128((__m128i*)&src[c]);
+        tmp = _mm_packus_epi16(tmp, tmp);
+        _mm_storel_epi64((__m128i*)&dst[c], tmp);
+      }
+      for (; c < hsize; ++c) {
+        dst[c] = src[c];
+      }
+      src += sstride;
+      dst += dstride;
     }
   }
 }
