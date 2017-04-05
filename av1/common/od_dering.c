1fb74191928849d4164713daeae19f5bd8e7e8b2
Jean-Marc Valin <jmvalin@mozilla.com>
2017-04-05 17:08:59 -0400

    more aggressive clpf
    Change-Id: I5de5bd8312f74d45dedffd79827f8dfd2c3a5752

--- av1/common/od_dering.c
+++ av1/common/od_dering.c
@@ -294,7 +294,7 @@ void od_dering(uint8_t *dst, int dstride, uint16_t *y, uint16_t *in, int xdec,
     0, 1, 2, 3, 4, 5, 6, 8, 10, 12, 15, 19, 24, 29, 36, 44, 54, 63
   };
   static int clpf_strength_table[CLPF_STRENGTHS] = {
-    0, 1, 2, 3, 5, 8, 12
+    0, 1, 2, 3, 6, 10, 15
   };
   int threshold = (pli ? level_table_uv : level_table)[level] << coeff_shift;
   clpf_strength = clpf_strength_table[clpf_strength];
