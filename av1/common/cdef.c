806ffd231e23d002e23b8bd97cb2dc758b0aab5d
Jean-Marc Valin <jmvalin@mozilla.com>
2017-04-12 02:40:29 -0400

    add arg
    Change-Id: If19b2600b29b7358fde3c718e39e034670265152

--- av1/common/cdef.c
+++ av1/common/cdef.c
@@ -55,7 +55,7 @@ static int is_8x8_block_skip(MODE_INFO **grid, int mi_row, int mi_col,
 }
 
 int sb_compute_dering_list(const AV1_COMMON *const cm, int mi_row, int mi_col,
-                           dering_list *dlist) {
+                           dering_list *dlist, int filter_skip) {
   int r, c;
   int maxc, maxr;
   MODE_INFO **grid;
@@ -79,6 +79,7 @@ int sb_compute_dering_list(const AV1_COMMON *const cm, int mi_row, int mi_col,
   assert(r_step == 1 || r_step == 2);
   assert(c_step == 1 || c_step == 2);
 
+  if (filter_skip) {
   for (r = 0; r < maxr; r += r_step) {
     for (c = 0; c < maxc; c += c_step) {
       dlist[count].by = r >> r_shift;
@@ -87,6 +88,17 @@ int sb_compute_dering_list(const AV1_COMMON *const cm, int mi_row, int mi_col,
           is_8x8_block_skip(grid, mi_row + r, mi_col + c, cm->mi_stride);
       count++;
     }
+  }
+  } else {
+  for (r = 0; r < maxr; r += r_step) {
+    for (c = 0; c < maxc; c += c_step) {
+      if (!is_8x8_block_skip(grid, mi_row + r, mi_col + c, cm->mi_stride)) {
+        dlist[count].by = r >> r_shift;
+        dlist[count].bx = c >> c_shift;
+        count++;
+      }
+    }
+  }
   }
   return count;
 }
@@ -273,7 +285,7 @@ void av1_cdef_frame(YV12_BUFFER_CONFIG *frame, AV1_COMMON *cm,
       if ((level == 0 && clpf_strength == 0 && uv_level == 0 &&
            uv_clpf_strength == 0) ||
           (dering_count = sb_compute_dering_list(
-               cm, sbr * MAX_MIB_SIZE, sbc * MAX_MIB_SIZE, dlist)) == 0) {
+               cm, sbr * MAX_MIB_SIZE, sbc * MAX_MIB_SIZE, dlist, 1)) == 0) {
         dering_left = 0;
         continue;
       }
