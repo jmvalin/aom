48b87dee7cb83a039fd19376ee7f17c51379c880
Jean-Marc Valin <jmvalin@mozilla.com>
2017-04-14 15:32:45 -0400

    simplify
    Change-Id: I82e5bd9b980a3520b95aa2e03f8e54b0feeff77b

--- av1/common/od_dering.c
+++ av1/common/od_dering.c
@@ -179,10 +179,9 @@ void od_filter_dering_direction_4x4_c(uint16_t *y, int ystride,
 /* This table approximates x^0.16 with the index being log2(x). It is clamped
    to [-.5, 3]. The table is computed as:
    round(256*min(3, max(.5, 1.08*(sqrt(2)*2.^([0:17]+8)/256/256).^.16))) */
-static const int16_t OD_THRESH_TABLE_Q8[18+6] = {
+static const int16_t OD_THRESH_TABLE_Q8[14] = {
         0, 20, 40, 60, 80, 100,
-       128, 134, 150, 168, 188, 210, 234, 256, 256,
-       256, 256, 256, 256, 256,
+       120, 134, 150, 168, 188, 210, 234, 256
 };
 
 /* Compute deringing filter threshold for an 8x8 block based on the
@@ -194,8 +193,8 @@ static const int16_t OD_THRESH_TABLE_Q8[18+6] = {
 static INLINE int od_adjust_thresh(int threshold, int32_t var) {
   int v1;
   /* We use the variance of 8x8 blocks to adjust the threshold. */
-  v1 = OD_MINI(32767*4, var);
-  return (threshold * OD_THRESH_TABLE_Q8[OD_ILOG(v1)] + 128) >> 8;
+  v1 = var ? OD_MINI(13, 1 + get_msb(var)) : 0;
+  return (threshold * OD_THRESH_TABLE_Q8[v1] + 128) >> 8;
 }
 
 void copy_8x8_16bit_to_16bit_c(uint16_t *dst, int dstride, const uint16_t *src,
